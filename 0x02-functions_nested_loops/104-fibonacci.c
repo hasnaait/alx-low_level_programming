d9679e363f81cd31a672c7cbe17764a31b191050
HASNAA AITAABDEKMALK <hasnaa.aitaabdelmalk@um5r.ac.ma>
2023-07-18 18:05:26 -0700

    104 fibonacci edited

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints the first 98 Fibonacci numbers
+ *	starting with 1 and 2 followed by a new line
+ *
+ *	Solution was copied from Nobert Patrick
+ *	Wise, github handle: Trikcode
+ *
+ * Return: Always 0 (Success)
+*/
 
-int main() {
-    unsigned int a = 1;
-    unsigned int b = 2;
-    unsigned int next;
-    int count;
+int main(void)
+{
+	unsigned int a = 1;
+	unsigned int b = 2;
+	unsigned int next;
+	int count;
 
-    printf("%u, %u", a, b);
+	printf("%u, %u", a, b);
 
-    for (count = 2; count < 98; count++) {
-        next = a + b;
-        printf(", %u", next);
-        a = b;
-        b = next;
-    }
+	for (count = 2; count < 98; count++)
+	{
+		next = a + b;
+		printf(", %u", next);
+		a = b;
+		b = next;
+	}
 
-    printf("\n");
+	printf("\n");
 
-    return 0;
+	return (0);
 }
