d883f9be2fd2e81230e6879e4cb3a4c233666b38
HASNAA AITAABDEKMALK <hasnaa.aitaabdelmalk@um5r.ac.ma>
2023-07-20 14:26:24 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * more_numbers - print 0 - 14 ten times and you
+ *		can only use _putchar three times
+ *
+ * Return: Always 0 (Success)
+*/
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+			_putchar(i);
+	}
+	_putchar('\n');
+}
