88bac1c10a7a40cef79d22c20b6a98bea7f65069
HASNAA AITAABDEKMALK <hasnaa.aitaabdelmalk@um5r.ac.ma>
2023-07-24 13:39:36 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,9 +8,13 @@
 void puts_half(char *str)
 {
 	int i;
-	
+	int odd = 0;
 
-	for (i = (_strlen(str) - 1) / 2; i < _strlen(str); i++)
+	if (_strlen(str) % 2 != 0)
+	{
+		odd += 1;
+	}
+	for (i = (_strlen(str) + oE) / 2; i < _strlen(str); i++)
 	{
 		_putchar(str[i]);
 	}
