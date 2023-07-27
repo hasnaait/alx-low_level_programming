e9a5960221e54e0c38d520084c6077806439d469
HASNAA AITAABDEKMALK <hasnaa.aitaabdelmalk@um5r.ac.ma>
2023-07-27 14:41:34 -0700

    task 1 eddited

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,7 +13,8 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int j;
 
-	for (j = 0; dest[j] != '\0'; j++);
+	for (j = 0; dest[j] != '\0'; j++)
+		;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
