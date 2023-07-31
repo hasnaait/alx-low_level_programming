ef76c48711a6bf2844314277fa4a8faeaf372a78
HASNAA AITAABDEKMALK <hasnaa.aitaabdelmalk@um5r.ac.ma>
2023-07-31 14:42:21 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _memcpy - copies @n bytes from memory area @src
  *	to memory area @dest
@@ -15,7 +14,7 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i = 0;
+	unsigned int i;
 
 	while (i < n)
 	{
