697103ecf37b57a0cc81d3e37d1272bb70188723
HASNAA AITAABDEKMALK <hasnaa.aitaabdelmalk@um5r.ac.ma>
2023-09-04 15:56:54 -0700

    task 2 eddit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		lenth = 0;
-		while (text_content[lenth];)
+		while (text_content[lenth])
 		{	lenth++;
 		}
 	}
