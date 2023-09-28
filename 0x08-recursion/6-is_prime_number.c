48815f666877949f1781fda572434881a11f53b9
HASNAA AIT AABDELMALK <hasnaa.aitaabdelmalk@um5r.ac.ma>
2023-09-28 15:32:40 -0700

    seventh fun

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * is_prime_recursive - checks to see if number is prime
+ * @n:int
+ * @div:int
+ * Return:int
+ */
+
+int is_prime_recursive(int n, int div)
+{
+	if (n < 2)
+		return (0);
+	if (div == 1)
+		return (1);
+	if (n % div == 0)
+		return (0);
+	return (is_prime_recursive(n, divisor - 1));
+}
+
+/**
+ * is_prime_number - states if number is prime
+ * @n:int
+ * Return:int
+ */
+
+int is_prime_number(int n)
+{
+	return (is_prime_recursive(n, n / 2));
+}
