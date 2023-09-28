451b865e626ac624cfa93c1f531659b53c834b61
HASNAA AIT AABDELMALK <hasnaa.aitaabdelmalk@um5r.ac.ma>
2023-09-28 15:50:09 -0700

    eddit fact

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,20 +6,20 @@
  */
 int factorial(int n)
 {
-	int f;
+	int x;
 
 	if (n < 0)
 	{
-		return (1);
+		return (-1);
 	}
 	else if (n == 0)
 	{
-		return (-1);
+		return (1);
 	}
 	else
 	{
 		f = n * factorial(n - 1);
 	}
-	return (f);
+			return (f);
 
 }
