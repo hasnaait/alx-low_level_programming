1e5048be83e99d098979b5099a753ba40463d562
HASNAA AIT AABDELMALK <hasnaa.aitaabdelmalk@um5r.ac.ma>
2023-10-02 15:03:28 -0700

    eddit add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,57 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
- * _isString - checks if the arg isstring
- * @s: string to check
- * Return: boolean
+ * main - it all starts here
+ * @argc: the number of arguments
+ * @argv: array of pointers to arguments
+ *
+ * Return: Always 0
  */
-int _isString(char *s)
+
+int main(int argc, char *argv[])
 {
-	int i = 0;
+	int sum = 0;
+	char *c;
 
-	for (; s[i] != '\0'; i++)
+	while (--argc)
 	{
-		if (!isdigit(s[i]))
-		{
-			return (0);
-		}
+		for (c = argv[argc]; *c; c++)
+			if (*c < '0' || *c > '9')
+				return (printf("Error\n"), 1);
+		sum += atoi(argv[argc]);
 	}
-	return (1);
-}
-/**
- * main - main function
- * @argc: argumentc
- * @argv: vector of arguments
- *Return: always 0
- */
-int main(int argc, char  *argv[])
-{
-	int i;
-	int result = 0;
-
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			if (_isString(argv[i]))
-			{
-				result += atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		printf("%d\n", result);
-		return (0);
-	}
-	else
-	{
-		printf("%d\n", 0);
-		return (1);
-	}
-
+	printf("%d\n", sum);
+	return (0);
 }
